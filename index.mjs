838e9c541022c9c9fe2872faed6b8b7888c3350a
Jan Brynda <jan.brynda@email.cz>
2022-02-28 21:11:46 +0100

    first_commit

---
+++ index.mjs
@@ -0,0 +1,38 @@
+import got from "got";
+import cheerio from "cheerio";
+
+const extractLinks = async (url) => {
+  try {
+    // Fetching HTML
+    const response = await got(url);
+    const html = response.body;
+
+    const $ = cheerio.load(html);
+
+    const linkObjects = $("a");
+
+    // Collect the "href" and "name" and "date" of each link and add them to an array
+    const links = [];
+    linkObjects.each((index, element) => {
+      try {
+        if ($(element).attr("href").includes("makro-letaky-a-katalogy")) {
+          links.push({
+            name: $(element).find("strong").text(),
+            date: $(element).find("span").last().text(),
+            href: $(element).attr("href"),
+          });
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    });
+
+    console.log(links);
+    return links;
+  } catch (error) {
+    console.log(error.response.body);
+  }
+};
+
+const URL = "https://www.makro.cz/aktualni-nabidka";
+extractLinks(URL);
